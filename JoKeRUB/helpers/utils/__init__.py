a3c6f4bd6c6d4944a44bb57cdf9e410d67836056
Mhmd26 <hamodey45@outlook.com>
2024-11-23 16:13:07 +0300

    Update __init__.py

--- JoKeRUB/helpers/utils/__init__.py
+++ JoKeRUB/helpers/utils/__init__.py
@@ -5,15 +5,18 @@ flag = True
 check = 0
 while flag:
     try:
+        # تأكد من أن الملفات هذه موجودة ومرتبة بشكل صحيح
         from . import format as _format
-        from . import tools as _cattools
+        from . import tools as _cattools  # تأكد من أن هذا الملف موجود
         from . import utils as _catutils
         from .events import *
         from .format import *
-
+        
         break
     except ModuleNotFoundError as e:
+        print(f"خطأ في استيراد {e.name} ... جاري تثبيت المكتبة.")
         install_pip(e.name)
         check += 1
         if check > 5:
+            print("فشلت عملية التثبيت بعد 5 محاولات.")
             break
