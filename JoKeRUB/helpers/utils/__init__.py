4f1ef5f7065c643e768521f04cf673f636029222
Mhmd26 <hamodey45@outlook.com>
2024-12-19 02:20:35 +0300

    Update __init__.py

--- JoKeRUB/helpers/utils/__init__.py
+++ JoKeRUB/helpers/utils/__init__.py
@@ -1,8 +1,12 @@
 from .extdl import *
 from .paste import *
 
+def install_pip(package_name):
+    import subprocess
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
+
 try:
-    # استيراد الوحدات
+    # محاولة استيراد الوحدات
     from . import format as _format
     from . import tools as _cattools
     from . import utils as _catutils
@@ -11,7 +15,7 @@ try:
 except ModuleNotFoundError as e:
     print(f"Module {e.name} not found. Attempting to install it...")
     try:
-        # تثبيت الوحدة المفقودة
+        # محاولة تثبيت الوحدة المفقودة
         install_pip(e.name)
         # إعادة محاولة الاستيراد بعد التثبيت
         from . import format as _format
