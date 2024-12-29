b8704c86e8b0a25f235e670fa80285e289c22bd9
Mhmd26 <hamodey45@outlook.com>
2024-12-30 02:13:04 +0300

    Update سوشل.py

--- JoKeRUB/plugins/يوتيوب.py
+++ JoKeRUB/plugins/يوتيوب.py
@@ -390,46 +390,7 @@ async def _(event):
         await bot.delete_dialog(conv.chat_id)
         await event.delete()
                 
-@l313l.on(admin_cmd(pattern="يوت(?: |$)(.*)"))
-async def _(event):
-    if event.fwd_from:
-        return
-    r_link = event.pattern_match.group(1)
 
-    await event.edit("**✎┊‌تتم المعالجة انتظر قليلا**")
-    chat = "@LEbot"  # تغيير اسم البوت إلى البوت المناسب لـ YouTube
-    async with bot.conversation(chat) as conv:
-        try:
-            msg_start = await conv.send_message("/start")
-            r = await conv.get_response()
-            msg = await conv.send_message(r_link)
-            details = await conv.get_response()
-
-            # التفاعل مع الأزرار
-            buttons = details.buttons
-            if buttons:
-                for row in buttons:
-                    for button in row:
-                        if button.text == "ملف صوتي":  # تأكد من النص الموجود على الزر
-                            await conv.send_message(button)
-                            audio_response = await conv.get_response()
-
-            video = await conv.get_response()
-            await bot.send_read_acknowledge(conv.chat_id)
-        except YouBlockedUserError:
-            await event.edit("✎┊‌الغـي حـظر هـذا البـوت و حـاول مجـددا @LEbot")
-            return
-
-        # إرسال الملف الصوتي
-        if audio_response:
-            await bot.send_file(event.chat_id, audio_response)
-
-        # حذف الرسائل الزائدة
-        await event.client.delete_messages(
-            conv.chat_id, [msg_start.id, r.id, msg.id, details.id, video.id]
-        )
-        await bot.delete_dialog(conv.chat_id)
-        await event.delete()
 
 @l313l.on(admin_cmd(pattern="سؤال(?: |$)(.*)"))
 async def _(event):
