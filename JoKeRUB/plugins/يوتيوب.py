0a79793c2964e3ad922cca32665735fb45f3230f
Mhmd26 <hamodey45@outlook.com>
2025-01-04 15:38:31 +0300

    Update يوتيوب.py

--- JoKeRUB/plugins/يوتيوب.py
+++ JoKeRUB/plugins/يوتيوب.py
@@ -337,7 +337,7 @@ async def _(event):
         await event.edit("**✎┊‌يجب وضع رابط الانستا مع الامر اولا **")
     else:
         await event.edit("**✎┊‌تتم المعالجة انتظر قليلا**")
-    chat = "@LEbot"  # تغيير اسم البوت إلى البوت المناسب لـ Instagram
+    chat = "@Biobot"  # تغيير اسم البوت إلى البوت المناسب لـ Instagram
     async with bot.conversation(chat) as conv:
         try:
             msg_start = await conv.send_message("/start")
@@ -348,7 +348,7 @@ async def _(event):
         
             await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await event.edit("✎┊‌الغـي حـظر هـذا البـوت و حـاول مجـددا @LEbot")
+            await event.edit("✎┊‌الغـي حـظر هـذا البـوت و حـاول مجـددا @Biobot")
             return
         await bot.send_file(event.chat_id, video)
         await event.client.delete_messages(
@@ -371,7 +371,7 @@ async def _(event):
         await event.edit("**✎┊‌ يجب وضع رابط الفيديو من التيك توك مع الامر  **")
     else:
         await event.edit("**✎┊‌تتم المعالجة انتظر قليلا**")
-    chat = "@LEbot"
+    chat = "@Biobot"
     async with bot.conversation(chat) as conv:
         try:
             msg_start = await conv.send_message("/start")
