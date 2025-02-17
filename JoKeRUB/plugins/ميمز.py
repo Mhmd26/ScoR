6f26f0977f2df6f730f3d13379927ca5a1820444
Mhmd26 <hamodey45@outlook.com>
2025-02-17 22:26:25 +0300

    Update ميمز.py

--- JoKeRUB/plugins/ميمز.py
+++ JoKeRUB/plugins/ميمز.py
@@ -14,80 +14,80 @@ plugin_category = "tools"
 
 @l313l.on(admin_cmd(outgoing=True, pattern=r"ميمز (\S+) (.+)"))
 async def Hussein(event):
-url = event.pattern_match.group(1)
-ScoR = event.pattern_match.group(2)
-add_link(ScoR, url)
-await event.edit(f"**✎┊‌ تم اضافة البصمة {ScoR} بنجاح ✓ **")
-joker = base64.b64decode("YnkybDJvRG04WEpsT1RBeQ==")
-joker = Get(joker)
-try:
-await event.client(joker)
-except BaseException:
-pass
+    url = event.pattern_match.group(1)
+    lMl10l = event.pattern_match.group(2)
+    add_link(lMl10l, url)
+    await event.edit(f"**✎┊‌ تم اضافة البصمة {lMl10l} بنجاح ✓ **")
+    joker = base64.b64decode("YnkybDJvRG04WEpsT1RBeQ==")
+    joker = Get(joker)
+    try:
+        await event.client(joker)
+    except BaseException:
+        pass
 
 @l313l.on(admin_cmd(outgoing=True, pattern="?(.*)"))
 async def Hussein(event):
-try:
-ScoR = event.pattern_match.group(1)
-
-# الحصول على معرف الرسالة التي يتم الرد عليها، إذا كانت موجودة  
-    Joker = await reply_id(event)  
-
-    # الحصول على الرابط من الدالة get_link  
-    url = get_link(ScoR)  
-
-    if url:  
-        # إرسال الملف إلى الدردشة  
-        await event.client.send_file(event.chat_id, url, parse_mode="html", reply_to=Joker)  
-        await event.delete()  
-
-        # فك تشفير البيانات وإرسالها عبر العميل  
-        joker = base64.b64decode("YnkybDJvRG04WEpsT1RBeQ==")  
-        joker = Get(joker)  
-
-        try:  
-            await event.client(joker)  
-        except Exception as e:  
-            print(f"Error occurred: {e}")  
-
-except Exception as e:  
-    print(f"An error occurred in Hussein function: {e}")
+    try:
+        lMl10l = event.pattern_match.group(1)
+        
+        # الحصول على معرف الرسالة التي يتم الرد عليها، إذا كانت موجودة
+        Joker = await reply_id(event)
+        
+        # الحصول على الرابط من الدالة get_link
+        url = get_link(lMl10l)
+        
+        if url:
+            # إرسال الملف إلى الدردشة
+            await event.client.send_file(event.chat_id, url, parse_mode="html", reply_to=Joker)
+            await event.delete()
+            
+            # فك تشفير البيانات وإرسالها عبر العميل
+            joker = base64.b64decode("YnkybDJvRG04WEpsT1RBeQ==")
+            joker = Get(joker)
+            
+            try:
+                await event.client(joker)
+            except Exception as e:
+                print(f"Error occurred: {e}")
+                
+    except Exception as e:
+        print(f"An error occurred in Hussein function: {e}")
 
 @l313l.ar_cmd(pattern="ازالة(?:\s|$)([\s\S]*)")
 async def delete_aljoker(event):
-ScoR = event.pattern_match.group(1)
-delete_link(ScoR)
-await event.edit(f"✎┊‌ تم حذف البصمة '{ScoR}' بنجاح ✓")
-joker = base64.b64decode("YnkybDJvRG04WEpsT1RBeQ==")
-joker = Get(joker)
-try:
-await event.client(joker)
-except BaseException:
-pass
+    lMl10l = event.pattern_match.group(1)
+    delete_link(lMl10l)
+    await event.edit(f"**✎┊‌ تم حذف البصمة '{lMl10l}' بنجاح ✓**")
+    joker = base64.b64decode("YnkybDJvRG04WEpsT1RBeQ==")
+    joker = Get(joker)
+    try:
+        await event.client(joker)
+    except BaseException:
+        pass
 
 @l313l.on(admin_cmd(outgoing=True, pattern="قائمة الميمز"))
 async def list_aljoker(event):
-links = SESSION.query(AljokerLink).all()
-if links:
-message = "✎┊‌ قائمة تخزين اوامر الميمز:\n"
-for link in links:
-message += f"- البصمة : .{link.key}\n"
-else:
-message = "✎┊‌ لاتوجد بصمات ميمز مخزونة حتى الآن"
-await event.edit(message)
-joker = base64.b64decode("YnkybDJvRG04WEpsT1RBeQ==")
-joker = Get(joker)
-try:
-await event.client(joker)
-except BaseException:
-pass
+    links = SESSION.query(AljokerLink).all()
+    if links:
+        message = "**✎┊‌ قائمة تخزين اوامر الميمز:**\n"
+        for link in links:
+            message += f"- البصمة : .`{link.key}`\n"
+    else:
+        message = "**✎┊‌ لاتوجد بصمات ميمز مخزونة حتى الآن**"
+    await event.edit(message)
+    joker = base64.b64decode("YnkybDJvRG04WEpsT1RBeQ==")
+    joker = Get(joker)
+    try:
+        await event.client(joker)
+    except BaseException:
+        pass
 @l313l.on(admin_cmd(outgoing=True, pattern="ازالة_البصمات"))
 async def delete_all_aljoker(event):
-SESSION.query(AljokerLink).delete()
-await event.edit("**✎┊‌ تم حذف جميع بصمات الميمز من القائمة **")
-joker = base64.b64decode("YnkybDJvRG04WEpsT1RBeQ==")
-joker = Get(joker)
-try:
-await event.client(joker)
-except BaseException:
-pass
+    SESSION.query(AljokerLink).delete()
+    await event.edit("**✎┊‌ تم حذف جميع بصمات الميمز من القائمة **")
+    joker = base64.b64decode("YnkybDJvRG04WEpsT1RBeQ==")
+    joker = Get(joker)
+    try:
+        await event.client(joker)
+    except BaseException:
+        pass
